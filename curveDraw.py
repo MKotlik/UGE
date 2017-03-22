6cfd44ed81ca677de4cdcb4610972ca3889d246b
Mikhail Kotlik <mkotlik@stuy.edu>
2017-03-22 11:27:11 -0400

    Everything works, changed 2 chars

--- curveDraw.py
+++ curveDraw.py
@@ -26,7 +26,7 @@ def add_circle(matrix, cX, cY, cZ, r, steps):
 def add_hermite(matrix, x0, y0, x1, y1, mX0, mY0, mX1, mY1, steps):
     print "hermite"
     # I'm not sure if im using this correctly...
-    hermite_mat = [[2, -3, 0, 1], [-2, 3, 0, 0], [1, -2, 1, 0], [1, 1, 0, 0]]
+    hermite_mat = [[2, -3, 0, 1], [-2, 3, 0, 0], [1, -2, 1, 0], [1,-1, 0, 0]]
     x_base = [[x0, x1, mX0, mX1]]
     y_base = [[y0, y1, mY0, mY1]]
     return add_general_curve(matrix, hermite_mat, x_base, y_base, steps)
@@ -35,7 +35,7 @@ def add_hermite(matrix, x0, y0, x1, y1, mX0, mY0, mX1, mY1, steps):
 def add_bezier(matrix, x0, y0, x1, y1, x2, y2, x3, y3, steps):
     print "bezier"
     # I'm not sure if im using this correctly...
-    bezier_mat = [[-1, 3, -3, 2], [3, -6, 3, 0], [-3, 3, 0, 0], [1, 0, 0, 0]]
+    bezier_mat = [[-1, 3, -3, 1], [3, -6, 3, 0], [-3, 3, 0, 0], [1, 0, 0, 0]]
     x_base = [[x0, x1, x2, x3]]
     y_base = [[y0, y1, y2, y3]]
     return add_general_curve(matrix, bezier_mat, x_base, y_base, steps)
@@ -52,7 +52,6 @@ def add_general_curve(matrix, inverse_mat, x_base, y_base, steps):
 
     while t < 1:
         X = (x_coeff[0] * t**3) + (x_coeff[1] * t**2) + (x_coeff[2] * t) + x_coeff[3]
-        print X
         Y = (y_coeff[0] * t**3) + (y_coeff[1] * t**2) + (y_coeff[2] * t) + y_coeff[3]
         add_edge(matrix, prevX, prevY, 0, X, Y, 0)
         prevX = X
